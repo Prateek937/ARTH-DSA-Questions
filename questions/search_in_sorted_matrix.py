1bca5fa21e173b46e9914779ec58b7978ff59801
Prateek <prateeksanodiya@gmail.com>
2021-10-26 19:15:44 +0530

    Questions

---
+++ questions/search_in_sorted_matrix.py
@@ -0,0 +1,25 @@
+'''
+Search in Sorted Matrix :
+
+You are given 2D array - Matrix of distinct integers and a target integer.
+Each row in the matrix is sorted, and each column is also sorted, the matrix does not necessarily have the same height and width
+
+Write a function that returns an array of the row and column indices of the target integer if its contained in the matrix, otherwise [ -1 , -1 ]
+
+Sample Input :
+
+matrix = [ 
+	[ 1 ,4 ,7 ,2, 15, 1000 ],
+	[ 2, 5, 19, 31, 32, 1001],
+	[ 3, 8, 24, 33, 35, 1002],
+	[ 40, 41, 42, 44, 45, 1003],
+	[ 99, 100, 103, 106, 128, 1004]
+]
+
+target = 44
+
+Sample output:
+[ 3 , 3 ]
+
+'''
+
